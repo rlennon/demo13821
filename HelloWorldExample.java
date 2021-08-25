9159216a1d7bc99ccce3edaefc2dd1be0c4eec29
Ruth Lennon <Ruth.Lennon@lyit.ie>
2021-08-25 19:46:16 +0100

    extra print statement

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -13,6 +13,7 @@
        */
         public static void main(String args[]){  
           System.out.println("Hello LYIT, again");  
+          System.out.println("Some silly change");
         }  
     }  
     
